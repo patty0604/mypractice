dfd86185b217e357a54d2218bd4301fb4cc49177
patty <patty860604@gmail.com>
2020-04-29 15:45:34 +0800

    phpsimplecode

---
+++ get_post/1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>get方式接收單行文字框與單行密碼框</title>
+    </head>
+    <body>
+    接收的單行文字框為<?php echo $_GET['username'];?><br>
+    接收的單行密碼框為<?php echo $_GET['passwd'];?><br>
+    </body>
+</html>
---
+++ get_post/2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    接收的文字框為
+    <?php echo $_POST['username'];?><br>
+    接收的密碼框為
+    <?php echo $_POST['passwd'];?>
+    
+</body>
+</html>
